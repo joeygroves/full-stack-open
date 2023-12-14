b17ab3c796b6e38c9291ddd0850e12f5c4862c5c
Joey Groves <link_groves@yahoo.co.uk>
2023-12-14 18:38:12 +0000

    added headings andbuttons

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,13 @@
 import { useState } from 'react'
 
+const Button = (props) => {
+  return (
+    <button>
+      {props.text}
+    </button>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -8,7 +16,11 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <h2>give feedback</h2>
+      <Button text="good"/>
+      <Button text="neutral"/>
+      <Button text="bad"/>
+      <h2>statistics</h2>
     </div>
   )
 }
