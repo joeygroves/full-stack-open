c22825f9fa6a1a393b27493cb9626fe4b9e88cfa
Joey Groves <link_groves@yahoo.co.uk>
2023-12-14 20:47:38 +0000

    finished 1.6 unicafe step1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
 
+const Display = props => <div>{props.heading} {props.statistic}</div>
+
 const Button = (props) => {
   return (
-    <button>
+    <button onClick={props.handleClick}>
       {props.text}
     </button>
   )
@@ -14,13 +16,31 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGood = () => {
+    setGood(good + 1);
+    console.log('new good value', good + 1);
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1);
+    console.log('new neutral value', neutral + 1);
+  }
+
+  const handleBad = () => {
+    setBad(bad + 1);
+    console.log('new bad value', bad + 1);
+  }
+
   return (
     <div>
       <h2>give feedback</h2>
-      <Button text="good"/>
-      <Button text="neutral"/>
-      <Button text="bad"/>
+      <Button handleClick={handleGood} text="good"/>
+      <Button handleClick={handleNeutral} text="neutral"/>
+      <Button handleClick={handleBad} text="bad"/>
       <h2>statistics</h2>
+      <Display heading="good" statistic={good} />
+      <Display heading="neutral" statistic={neutral} />
+      <Display heading="bad" statistic={bad} />
     </div>
   )
 }
