28c562683c2209c14822efe4b26627a5b8155c0b
Joey Groves <link_groves@yahoo.co.uk>
2024-02-07 11:35:53 +0000

    started the post request

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
       "id": 1,
@@ -57,6 +59,12 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter(persons => persons.id !== id)
 
   response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const person = request.body
+  console.log(person)
+  response.json(person)
 })
 
 const PORT = 3001
