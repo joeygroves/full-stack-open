67b3302d10ca6818d6e83104c8f83864389bd3ea
Joey Groves <link_groves@yahoo.co.uk>
2024-02-24 20:49:26 +0000

    started 3.12

---
+++ part3/mongo.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.kr6y3o1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'HTML is easy',
+  important: true,
+})
+
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
