92c3c802d0ade2cec3629c06ad5e3724df72bad9
Joey Groves <link_groves@yahoo.co.uk>
2024-02-26 19:45:39 +0000

    finished 3.13

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 app.use(cors())
 
@@ -41,7 +43,9 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/info', (request, response) => {
